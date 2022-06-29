Corrected pose calculation bug and refactored.

FourBar/FourBarMain.py
@@ -58,15 +58,16 @@ class MainWindow(QMainWindow, FourBarUI.Ui_MainWindow):
 
         # Mechanism encoding: [lengths,base1,base2]
         self.initialMechanismBases = [[0.06,0.04],[1.5,-0.02]]
-        baselength = self.vLen(self.initialMechanismBases[0],self.initialMechanismBases[1])
+        baselength = fb.vLen(self.initialMechanismBases[0],self.initialMechanismBases[1])
         #self.initialMechanismLengths = [baselength,0.3,0.75,0.6,1,1.5]
-        self.initialMechanismLengths = [0.25, 1.1,0.83,1.25,1.46]
+        self.initialMechanismLengths = [.8, 1.2,1.1,1,1]
         self.initialAlpha = 1.5
 
         # Four Bar Properties
         self.mechanismBases = self.initialMechanismBases
         self.mechanismLengths = self.initialMechanismLengths
-        self.mechanismPoints = fb.calculateFourBarPoint([self.mechanismBases, self.mechanismLengths], self.initialAlpha)
+        self.mechanism = [self.mechanismBases, self.mechanismLengths]
+        self.mechanismPoints = fb.calculateFourBarPoint(self.mechanism, self.initialAlpha)
 
         # Four bar plotting
         self.plotAngles = np.linspace(0.0,2*np.pi,num=360).tolist()
@@ -77,6 +78,7 @@ class MainWindow(QMainWindow, FourBarUI.Ui_MainWindow):
         # UI Connections
         self.programLoaded = False
         self.dampingSlider.valueChanged.connect(self.dampingChanged)
+        self.angleSlider.valueChanged.connect(self.angleChanged)
         self.inputTable.itemSelectionChanged.connect(self.selectedInputTable)
         self.inputTable.cellChanged.connect(self.inputCellChanged)
         self.maxIterationsTextBox.textChanged.connect(self.maxIterationsChanged)
@@ -84,20 +86,19 @@ class MainWindow(QMainWindow, FourBarUI.Ui_MainWindow):
 
         # Display mechanims plot
         self.redraw_mechanism()
-
+        self.redrawResultsLabels()
         self.programLoaded = True
         self.userEdited = True
 
     def startOptimization(self):
         # Create calculation thread
-        self.design = self.packageDesign()
         controls = [self.damping,self.maxIterations]
         targets_to_send = self.targets
         for i in range(len(targets_to_send)):
             target = targets_to_send[i]
-            target[0] = target[0]*2*np.pi/360
+            target[0] = target[0]*np.pi/18
             targets_to_send[i] = target
-        self.optimizeThread = fb.OptimizeThread(self.design,self.targets,controls)
+        self.optimizeThread = fb.OptimizeThread(self.mechanism,targets_to_send,controls)
         # Connect to emitted signals
         self.optimizeThread.iterationDone.connect(self.iterationDone)
         self.optimizeThread.designOptimized.connect(self.designOptimized)
@@ -277,44 +278,36 @@ class MainWindow(QMainWindow, FourBarUI.Ui_MainWindow):
 
     def redraw_mechanism(self):
         # Display mechanims plot
+        print("Redrawing Mechanism")
+        print("Bases:")
+        print(self.mechanismBases)
+        print("Lengths:")
+        print(self.mechanismLengths)
         self.calculateActualPath(self.plotAngles)
         path = [self.pathX,self.pathY]
-        pose = fb.calculateFourBarPose([self.mechanismBases, self.mechanismLengths],self.initialAlpha)
-
+        try:
+            pose = fb.calculateFourBarPose([self.mechanismBases, self.mechanismLengths],self.initialAlpha)
+        except ValueError:
+            pose = None
+            print("Invalid pose")
         self.graph_canvas.plotFourBar(self.targets, pose,path)
 
-
-    def parseDesign(self,design):
-        print('Do we need to parse the design?')
-
-    def packageDesign(self):
-        # Mechanism encoding: [lengths,base1,base2]
-        return self.mechanismLengths + self.mechanismBases[0] + self.mechanismBases[1]
-
     def iterationDone(self,design):
-        self.mechanismBases = [
-            [design[5],design[6]],
-            [design[7],design[8]]
-        ]
-        self.mechanismLengths = list(design[0:5])
+        print("Iteration Done")
+        self.mechanismBases = design[0]
+        self.mechanismLengths = design[1]
+        self.mechanism = [self.mechanismBases, self.mechanismLengths]
+
         #self.progress = int(design[2]*100)
-        print(design)
+        #print(design)
         self.redrawResults()
         self.resultsBar.setValue(self.progress)
 
     def designOptimized(self,design):
+        self.optimizeThread.terminate
         self.startButton.setEnabled(True)
         self.stopButton.setEnabled(False)
-        self.optimizeThread.terminate
         #print(design)
-
-    def vLen(self,point1,point2):
-        # takes in two points in the format [x,y] and returns the float of vector length
-        dx = point1[0] - point2[0]
-        dy = point1[1] - point2[1]
-
-        length = np.sqrt(dx*dx + dy*dy)
-        return length
 
     def calculateActualPath(self,angleList):
         xActual = [0]*len(angleList)
@@ -334,6 +327,16 @@ class MainWindow(QMainWindow, FourBarUI.Ui_MainWindow):
         for target in self.targets:
             if target[3] == True:
                 self.exactSelected = self.exactSelected + 1
+
+    def angleChanged(self, angle):
+        self.initialAlpha = angle * np.pi/ 180
+        self.angleLabel.setText("Angle = %i"%(self.initialAlpha*180 / np.pi))
+        try:
+            pose = fb.calculateFourBarPose([self.mechanismBases, self.mechanismLengths],self.initialAlpha)
+        except ValueError:
+            pose = fb.dummy_pose([self.mechanismBases, self.mechanismLengths], self.initialAlpha)
+            print("Invalid pose")
+        self.graph_canvas.plotFourBar(self.targets, pose, [self.pathX,self.pathY])
 
 def main():
     app = QApplication(sys.argv)

FourBar/FourBarOptimize.py
@@ -12,7 +12,7 @@ class OptimizeThread(QThread):
     designOptimized = QtCore.pyqtSignal(object)
     def __init__(self, design, targets, controls):
         QThread.__init__(self)
-        self.design = list(design)
+        self.design = design
         self.optimizedDesign = self.design
         self.targets = targets
 
@@ -38,19 +38,19 @@ class OptimizeThread(QThread):
         nMax = self.maxIterations
         #while rp < rpMax:
             #n = n+1.0
-        (error,optimumParameters) = self.steepestDescentMinimum(
+        (error,optimumParameters) = steepestDescentMinimum(
             self.evaluateObjectiveFunction,
-            list(self.optimizedDesign),
+            design2list(self.optimizedDesign),
             nMax = self.maxIterations,
             damping=self.damping)
 
         # Return to main thread
         #progress = float(n/nMax)
-        self.optimizedDesign = optimumParameters
+        self.optimizedDesign = list2design(optimumParameters)
         self.iterationDone.emit(self.optimizedDesign)
         #rp = rp*1.5
         self.objectiveSequence.append(error)
-
+        print(rounded_list(optimumParameters,3))
         return self.optimizedDesign
 
     def run(self):
@@ -59,19 +59,30 @@ class OptimizeThread(QThread):
 
         self.sleep(2)
 
-def evaluateObjectiveFunction(self,design,targets,rp=1):
+    def evaluateObjectiveFunction(self, design,rp=1):
+        targets = self.targets
+        objective = 0.0
+        for target in targets:
+            # Calculate position at crank angle target[0]
+            try:
+                position = calculateFourBarPoint(design,target[0])
+                objective = objective + vLen(target[1:],position)**2
+            except ValueError:
+                objective += 2
 
-    objective = 0.0
-    for target in targets:
-        # Calculate position at crank angle target[0]
-        try:
-            position = self.calculateFourBarPoint(design,target[0])
-            objective = objective + self.vLen(target[1:],position)**2
-        except ValueError:
-            objective += 999
+        #TODO: Add lengths of bars as minor addition to objective function
+        lengths = design[4:]
+        #print(lengths)
+        objective = objective + sum(lengths)/1000
+        return objective
 
-    #TODO: Add lengths of bars as minor addition to objective function
-    return objective
+def design2list(design):
+    return design[0][0] + design[0][1] + design[1]
+
+def list2design(param_list):
+    bases = [param_list[0:2], param_list[2:4]]
+    lengths = param_list[4:]
+    return [bases, lengths]
 
 def calculateFourBarPose(design, alpha, link_positive=True, dyad_positive=True):
     # lengths is in format [base, z1,z2,z3,z4,z5]
@@ -95,47 +106,77 @@ def calculateFourBarPose(design, alpha, link_positive=True, dyad_positive=True):
     # all lengths > 0
     # Sum of two Dyad legs >= dyad base
 
-    #        C
-    #       / \
-    #     /    \
-    #    1--------2
-    #   /         \
-    #  /          \
-    # B1           B2
+    #         C
+    #     3  / \ 4
+    #      /    \
+    #     1--------2
+    # 0  /    1    \
+    #   /          \ 2
+    #  B1---- 5 --- B2
     bases = design[0]
-    length = design[1]
-    length.insert(0,vLen(bases[0],bases[1]))
+    length_without_base = design[1]
+
+    lengths = length_without_base + [vLen(bases[0],bases[1])]
 
     # Crank Endpoint
-    point1 = [0,0]
-    point1[0] = bases[0][0] + length[1]*np.cos(alpha)
-    point1[1] = bases[0][1] + length[1]*np.sin(alpha)
+    point1 = vector_add_length_angle(bases[0], lengths[0], alpha)
 
     try:
-        point2options = link_intersections(bases[0], bases[1], length[2], length[3])
+        point2options = link_intersections(point1, bases[1], lengths[1], lengths[2])
         if link_positive:
             point2 = point2options[0]
         else:
             point2 = point2options[1]
     except ValueError:
-        raise ValueError("Mechanism is not solvable in this configuration")
+        raise ValueError("Mechanism base is not solvable in this configuration")
 
     try:
-        pointCoptions = link_intersections(point1, point2, length[4], length[5])
+        pointCoptions = link_intersections(point1, point2, lengths[4], lengths[5])
         if dyad_positive:
             pointC = pointCoptions[0]
         else:
             pointC = pointCoptions[1]
     except ValueError:
-        raise ValueError("Mechanism is not solvable in this configuration")
+        raise ValueError("Mechanism dyad is not solvable in this configuration")
 
     mechanismPoints = [bases[0],bases[1],point1,point2,pointC]
+    return mechanismPoints
+
+def vector_add_length_angle(start, length, angle):
+    # Start is a 2-element list of x,y coordinates
+    # Length is a value
+    # Angle is a value in radians
+    end = [0,0]
+    end[0] = start[0] + length*np.cos(angle)
+    end[1] = start[1] + length*np.sin(angle)
+    return end
+
+def dummy_pose(design,alpha):
+    bases = design[0]
+    length_without_base = design[1]
+
+    lengths = [vLen(bases[0],bases[1])] + length_without_base
+
+    # Crank Endpoint
+    point1 = [0,0]
+    point1[0] = bases[0][0] + lengths[1]*np.cos(alpha)
+    point1[1] = bases[0][1] + lengths[1]*np.sin(alpha)
+    mechanismPoints = [bases[0],bases[1],point1,bases[0],bases[0]]
     return mechanismPoints
 
 def calculateFourBarPoint(design,alpha):
     # lengths is in format [base, z1,z2,z3,z4,z5]
     # bases are in format [[base1X,base1Y],[base2X,base2Y]]
     # alpha is a single float for crank angle from horizontal
+
+    # Assumes Nested list design structure
+    if len(design) != 2:
+        optimizing_mode = True
+        #print("Making design a list in calculateFourBarPoint")
+        design = list2design(design)
+    else:
+        design = list(design)
+
     try:
         pose = calculateFourBarPose(design, alpha)
     except ValueError:
@@ -233,7 +274,7 @@ def minimizeParabola(c):
     # Outputs: Values of x and y where y is minimized
     minX = -c[1]/(2*c[2])
 
-    minY = self.getValueOfPoly(c,minX)
+    minY = getValueOfPoly(c,minX)
     return (minX,minY)
 
 def getValueOfPoly(c,x):
@@ -291,7 +332,7 @@ def steepestDescentMinimum(function,
         i = i+1
         # Get gradient at position
         # print("About to get gradient")
-        slopeList = self.gradient(function,position)
+        slopeList = gradient(function,position)
         # print("fitting polynomial...")
         # Get three points in that direction at positions of alpha
         functionValues = []
@@ -301,7 +342,7 @@ def steepestDescentMinimum(function,
                 testLocation.append(oldPosition-slope*alphaValue)
             functionValues.append(function(testLocation))
         # Fit parabola to curve
-        C = self.threePointQuadraticApprox(alpha, functionValues)
+        C = threePointQuadraticApprox(alpha, functionValues)
         # Check parabola is concave up
         # Calculate alpha that gives minimum
         alphaStar = 0.0
@@ -312,7 +353,7 @@ def steepestDescentMinimum(function,
             print("Shallow gradient, using constant step size")
             alphaStar = constantStepSize
         else:
-            (alphaStar,bestY) = self.minimizeParabola(C)
+            (alphaStar,bestY) = minimizeParabola(C)
         # Move to position of calculated alpha
         newPosition = []
         for oldPosition, slope in zip(position,slopeList):
@@ -430,3 +471,6 @@ def angle_from_triangle_lengths(a,b,c):
     acos_input = ((b*b) + (c*c) - (a*a)) / (2*b*c)
     alpha = math.acos(acos_input)
     return alpha
+
+def rounded_list(xs, places):
+    return [round(x,places) for x in xs]